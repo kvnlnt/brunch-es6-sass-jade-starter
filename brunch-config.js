ef870530cae8b91ae6336d8f3ebe662c7bc104fc
Kevin Lint <kevinlint@toolhouse.com>
2017-12-19 15:59:26 -0800

    init

---
+++ brunch-config.js
@@ -0,0 +1,15 @@
+// See http://brunch.io for documentation.
+exports.files = {
+  javascripts: {
+    joinTo: {
+      "module.js": /^app/
+    }
+  },
+  stylesheets: {
+    joinTo: "module.css"
+  }
+};
+
+exports.plugins = {
+  babel: { presets: ["latest"] }
+};
